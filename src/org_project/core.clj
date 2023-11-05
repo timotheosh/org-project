55d9d3260a3a49aa02e2a005eac1c9cdf3e8679b
Tim Hawes <trhawes@gmail.com>
2023-11-05 18:54:14 -0500

    Find line prefix with regex rather than depending on proper space delimiting.

--- src/org_project/core.clj
+++ src/org_project/core.clj
@@ -1,52 +1,78 @@
 (ns org-project.core
   (:require [clojure.string :as string]))
 
-(defn split-by-headers [text]
+(defn split-by-headers
+  "Splits a document fed in as a single string on org-mode headers."
+  [text]
   (let [pattern #"(?<=\n)(?=\*+ )"]
-    (->> (clojure.string/split text pattern)
-         (remove clojure.string/blank?)
+    (->> (string/split text pattern)
+         (remove string/blank?)
          (map #(string/trim %)))))
 
 ;; (def org-file (parse (slurp "/home/thawes/org-files/Projects/naurrnen-website/src/index.org")))
 (def org-file (split-by-headers (slurp "/home/thawes/org-files/Projects/naurrnen-website/src/index.org")))
 
-(defn key-prefix [line]
-  (let [index (string/index-of line " ")
-        eol (string/index-of line "\n")]
-    (cond  (and (nil? eol) (nil? index)) (keyword (string/lower-case line))
-           (or (and (nil? index) eol) (and (and index eol) (> index eol))) (keyword (string/lower-case (subs line 0 eol)))
-           :else (keyword (string/lower-case (subs line 0 index))))))
+(defn old-key-prefix
+  "Determines the keyword for org-mode directives or section headings."
+  [line]
+  (let [trimmed (clojure.string/trim line)
+        section-pattern #"^(\*+)\s"
+        directive-pattern #"^\#\+([a-zA-Z_]+):"]
+    (cond
+      (re-find section-pattern trimmed)
+      (->> trimmed
+           (re-find section-pattern)
+           first
+           clojure.string/lower-case
+           keyword)
 
-(defn remove-prefix [line]
-  (let [index (string/index-of line " ")]
+      (re-find directive-pattern trimmed)
+      (->> trimmed
+           (re-find directive-pattern)
+           second
+           clojure.string/lower-case
+           keyword)
+
+      :else :string)))
+
+(defn key-prefix
+  "Determines the keyword for org-mode directives or section headings."
+  [line]
+  (let [trimmed (clojure.string/trim line)
+        section-pattern #"^(\*+)\s.*" ; Match only asterisks at the beginning
+        directive-pattern #"^\#\+([a-zA-Z_]+):.*"] ; Match only the directive name
+    (cond
+      (re-find section-pattern trimmed)
+      (->> trimmed
+           (re-find section-pattern)
+           second ; Get the group of asterisks
+           clojure.string/lower-case
+           keyword)
+
+      (re-find directive-pattern trimmed)
+      (->> trimmed
+           (re-find directive-pattern)
+           second ; Get the group with the directive name
+           clojure.string/lower-case
+           keyword)
+
+      :else :string)))
+
+
+
+(defn remove-prefix
+  "Removed the prefix from the string and return the result. This is usually org-mode that needs to still be parsed."
+  [line]
+  (let [trimmed (string/trim line)
+        index (string/index-of trimmed " ")]
     (if (nil? index)
-      line
-      (subs line (inc index)))))
+      trimmed
+      (subs trimmed (inc index)))))
 
 (defn split-line [line]
   (let [index (string/index-of line "\n")]
     (if index
       (list (subs line 0 index) (subs line (inc index))))))
-
-(defn orig-get-properties [line]
-  (let [begin ":PROPERTIES:"
-        end ":END:"
-        start-index (+ (count begin) (string/index-of line begin))
-        end-index (string/index-of line end)
-        content-index (+ (count end) end-index)]
-    (when (> start-index end-index)
-      (throw (Throwable. "MALFORMED PROPERTIES")))
-    (let [props-string (string/trim (subs line start-index end-index))
-          props (-> props-string
-                    (string/split #"\n")
-                    ((fn [x]
-                       (map #(string/split % #" ") x)))
-                    ((fn [x]
-                       (map (fn [y] [(-> (first y)
-                                         string/lower-case
-                                         (string/replace #":" "")
-                                         keyword) (second y)]) x))))]
-      (assoc (into (sorted-map) props) :content (string/trim (subs line content-index))))))
 
 (defn parse-property-line [line]
   (let [parts (string/split line #" ")]
@@ -85,8 +111,10 @@
 
 (defmethod hiccupify :* [block]
   (let [section (split-line block)
-        heading (remove-prefix (first section))]
-    [:div {:class "section-1" :id heading} [:h1 heading] (hiccupify (second section))]))
+        heading (remove-prefix (first section))
+        content-with-props (get-properties (second section))]
+    [:div {:class "section-1" :id heading} [:h1 heading]
+     ]))
 
 (defmethod hiccupify :** [block]
   (let [section (split-line block)
@@ -98,6 +126,12 @@
         heading (remove-prefix (first section))]
     [:div {:class "section-1" :id heading} [:h3 heading] (hiccupify (second section))]))
 
+(defmethod hiccupify :properties [block]
+  (handle-properties block))
 
-(defmethod hiccupify :default [block]
+(defmethod hiccupify :string [block]
   [:p block])
+
+(defmethod hiccupify :default
+  [block]
+  (str "<!-- " block "-->"))
--- test/org_project/core_test.clj
+++ test/org_project/core_test.clj
@@ -2,6 +2,35 @@
   (:require [clojure.test :refer :all]
             [org-project.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-heirachical-split
+  (testing ""
+    (is (=  (split-by-headers "#+title: Welcome\n\n* Section 1\n\nNullam libero mauris.\n** Subsection 1\nOnsequat quis, varius et, dictum id, arcu.\n* Section 2\nSed id ligula quis est convallis tempor.")
+            '("#+title: Welcome"
+              "* Section 1\n\nNullam libero mauris."
+              "** Subsection 1\nOnsequat quis, varius et, dictum id, arcu."
+              "* Section 2\nSed id ligula quis est convallis tempor.")))))
+
+(deftest test-key-prefix
+  (testing "org-mode directives"
+    (is (= (key-prefix "#+title: My Document") :title))
+    (is (= (key-prefix "#+TITLE: My Document") :title)) ; case insensitivity
+    (is (= (key-prefix "#+results:") :results)))
+
+  (testing "section headings"
+    (is (= (key-prefix "* First level heading") :*))
+    (is (= (key-prefix "** Second level heading") :**))
+    (is (= (key-prefix "*** Third level heading") :***)))
+
+  (testing "lines that are not directives or headings"
+    (is (= (key-prefix "Just some text.") :string))
+    (is (= (key-prefix "Another line of text") :string))
+    (is (= (key-prefix "# Not a directive") :string))
+    (is (= (key-prefix "****Not a section because of the missing space") :string)))
+
+  (testing "empty lines"
+    (is (= (key-prefix "") :string))
+    (is (= (key-prefix " ") :string)))
+
+  (testing "lines with only whitespace before directive or heading"
+    (is (= (key-prefix "  #+options: toc:nil") :options))
+    (is (= (key-prefix "   * Whitespace before heading") :*))))
